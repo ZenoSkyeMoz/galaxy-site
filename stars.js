069de7bdfe7dde96523891cadd12e1cef25ed447
ZenoSkyeMoz <CODMSHEESHROBLOX@gmail.com>
2025-08-07 18:44:48 +0800

    Update stars.js

--- stars.js
+++ stars.js
@@ -1,43 +1,42 @@
 const canvas = document.getElementById("stars");
 const ctx = canvas.getContext("2d");
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let stars = [];
-for (let i = 0; i < 150; i++) {
+
+for (let i = 0; i < 200; i++) {
   stars.push({
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
-    r: Math.random() * 1.5,
-    d: Math.random() * 1
+    radius: Math.random() * 1.5,
+    speed: Math.random() * 0.6 + 0.2
   });
 }
 
 function drawStars() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "#ffffff";
+  ctx.fillStyle = "white";
   ctx.shadowBlur = 5;
   ctx.shadowColor = "white";
-  for (let i = 0; i < stars.length; i++) {
-    let s = stars[i];
+
+  stars.forEach(star => {
     ctx.beginPath();
-    ctx.arc(s.x, s.y, s.r, 0, 2 * Math.PI);
+    ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
     ctx.fill();
-  }
-  moveStars();
-}
 
-function moveStars() {
-  for (let i = 0; i < stars.length; i++) {
-    stars[i].y += stars[i].d;
-    if (stars[i].y > canvas.height) {
-      stars[i].y = 0;
-      stars[i].x = Math.random() * canvas.width;
+    star.y += star.speed;
+    if (star.y > canvas.height) {
+      star.y = 0;
+      star.x = Math.random() * canvas.width;
     }
-  }
+  });
+
+  requestAnimationFrame(drawStars);
 }
 
-setInterval(drawStars, 33);
+drawStars();
 
 window.addEventListener("resize", () => {
   canvas.width = window.innerWidth;
